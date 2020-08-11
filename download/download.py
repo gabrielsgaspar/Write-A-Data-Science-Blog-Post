dde1ff50d3d66fbdefeb3bff7e366f63d1cf8476
Gabriel Simoes Gaspar <gabrielsgaspar@gmail.com>
2020-08-11 18:03:34 +0100

    Update download.py

--- download/download.py
+++ download/download.py
@@ -7,30 +7,30 @@ the data folder
 """
 
 # Import libraries
-import os
-import requests
+import itertools
+from google_drive_downloader import GoogleDriveDownloader as gdd
 
-
-def get_data(path):
+# Define function to download data
+def get_data(path, ids, names):
     ''' Downloads csv files for all available Stack Overflow
         Annual Developer Survey years and saves in specified path
     '''
-    # Set url for survey years
-    url_2020 = 'https://drive.google.com/file/d/1dfGerWeWkcyQ9GX9x20rdSGj7WtEpzBB/view?usp=sharing'
-    url_2019 = 'https://drive.google.com/open?id=1QOmVDpd8hcVYqqUXDXf68UMDWQZP0wQV'
-    url_2018 = 'https://drive.google.com/uc?export=download&id=1_9On2-nsBQIw3JiY43sWbrF8EjrqrR4U'
-    url_2017 = 'https://drive.google.com/uc?export=download&id=0B6ZlG_Eygdj-c1kzcmUxN05VUXM'
-    url_2016 = 'https://drive.google.com/uc?export=download&id=0B0DL28AqnGsrV0VldnVIT1hyb0E'
-    url_2015 = 'https://drive.google.com/uc?export=download&id=0B0DL28AqnGsra1psanV1MEdxZk0'
-    url_2014 = 'https://drive.google.com/uc?export=download&id=0B0DL28AqnGsrempjMktvWFNaQzA'
-    url_2013 = 'https://drive.google.com/uc?export=download&id=0B0DL28AqnGsrenpPNTc5UE1PYW8'
-    url_2012 = 'https://drive.google.com/uc?export=download&id=0B0DL28AqnGsrX3JaZWVwWEpHNWM'
-    url_2011 = 'https://drive.google.com/uc?export=download&id=0Bx0LyhBTBZQgUGVYaGx3SzdUQ1U'
-    # Donwload data
-    ## # TODO:
-    # Save data
-    ## # TODO:
+    # Donwload and save data
+    for (id, name) in zip(ids, names):
+        gdd.download_file_from_google_drive(file_id = id, dest_path = path + name, unzip = False)
 
-if __name == '__main__':
-    path = os.chdir("../data")
-    get_data()
+if __name__ == '__main__':
+    # Set path where data will be saved
+    path = '/Users/gabrielsgaspar/Projects/personal_projects/Write-A-Data-Science-Blog-Post/data/'
+    # Set file ids
+    ids = ['1dfGerWeWkcyQ9GX9x20rdSGj7WtEpzBB', '1QOmVDpd8hcVYqqUXDXf68UMDWQZP0wQV',
+            '1_9On2-nsBQIw3JiY43sWbrF8EjrqrR4U', '0B6ZlG_Eygdj-c1kzcmUxN05VUXM',
+            '0B0DL28AqnGsrV0VldnVIT1hyb0E', '0B0DL28AqnGsra1psanV1MEdxZk0',
+            '0B0DL28AqnGsrempjMktvWFNaQzA', '0B0DL28AqnGsrenpPNTc5UE1PYW8',
+            '0B0DL28AqnGsrX3JaZWVwWEpHNWM', '0Bx0LyhBTBZQgUGVYaGx3SzdUQ1U']
+    # Set file names
+    names = ['developer_survey_2020.zip', 'developer_survey_2019.zip', 'developer_survey_2018.zip',
+            'developer_survey_2017.zip', 'developer_survey_2016.zip', 'developer_survey_2015.zip',
+            'developer_survey_2014.zip', 'developer_survey_2013.zip', 'developer_survey_2012.zip',
+            'developer_survey_2011.zip']
+    get_data(path, ids, names)
