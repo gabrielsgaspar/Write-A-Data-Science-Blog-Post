e3ed1967cdf26c777c09b68fe0f72d28a2fdd42e
Gabriel Simoes Gaspar <gabrielsgaspar@gmail.com>
2020-08-25 01:22:55 +0100

    Fix unzip function

--- download/shape.py
+++ download/shape.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 The purpose of this script is to programatically download shapefiles
-from the World Bank database and save them
+from the IPSUM database and save them
 """
 
 # Import libraries
@@ -11,13 +11,13 @@ import os
 from zipfile import ZipFile
 
 # Define function to download shapefiles
-def download_shape(path):
+def download_shape(url, zip_name):
     '''Downloads zip files that contains shapefile data from
     IPSUM's website
     '''
-    url = 'https://international.ipums.org/international/resources/gis/IPUMSI_world_release2017.zip'
-    r = requests.get(url)
-    open(str(path + '/shapes.zip'), 'wb').write(r.content)
+    r = requests.get(url, allow_redirects = True)
+    with open(zip_name, 'wb') as file:
+        file.write(r.content)
 
 # Define function to unzip file
 def unzip_shp(zip_name, shp_name):
@@ -28,8 +28,8 @@ def unzip_shp(zip_name, shp_name):
     print('Unzipping', zip_name, '...', end = '\r')
     # Open zipfile and get only shapefile
     with ZipFile(zip_name) as zf:
-        with zf.open(shp_name, 'r') as infile:
-            # TODO
+        with open(shp_name, 'wb') as f:
+            f.write(zf.read(shp_name))
 
 # Define function to delete zip files
 def remove(ext):
@@ -45,7 +45,7 @@ def remove(ext):
         if file.endswith(ext):
             os.remove(os.path.join(dir, file))
 
-if __main__ == '__main__':
+if __name__ == '__main__':
     # Create data directory if it doesn't exist and change directory
     if not os.path.exists('../data/shapefile'):
         os.makedirs('../data/shapefile')
@@ -53,13 +53,12 @@ if __main__ == '__main__':
     else:
         os.chdir('../data/shapefile')
     # Set variables
+    url = 'https://international.ipums.org/international/resources/gis/IPUMSI_world_release2017.zip'
     zip_name = 'shapes.zip'
     shp_name = 'world_countries_2017.shp'
-    # Get working directory
-    path = str(os.getcwd())
     # Call function to download files
-    download_shape(path)
-    # Call functio to extract data
+    download_shape(url, zip_name)
+    # Call function to extract data
     unzip_shp(zip_name, shp_name)
     # Delete zip files
     remove('.zip')
